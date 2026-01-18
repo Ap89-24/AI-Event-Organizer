7417a737ac7569498c0fb681ba58fc577f6c7a12
Aman Patel <aman082199@gmail.com>
2026-01-18 18:25:24 +0530

    Shadcn setup

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
      <div>
-      <Button>Click Me</Button>
+      <Button variant="destructive">Click Me</Button>
      </div>
   );
 }
