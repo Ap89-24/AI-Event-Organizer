1e8d2d1f9364659bb51b015cc41daec834972d27
Aman Patel <aman082199@gmail.com>
2026-01-22 23:13:46 +0530

    Basic layouting of our app

---
+++ components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <nav>
+        <div>
+            {/* {logo} */}
+
+            {/* {search bar and location for desktop} */}
+
+            {/* {right side section} */}
+        </div>
+        {/* search and location for mobile only */}
+        
+      </nav>
+    </>
+  )
+}
+
+export default Header
