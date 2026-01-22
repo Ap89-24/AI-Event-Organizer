213d1f88789f90bb75b9ea23993f10cb61a481bf
Aman Patel <aman082199@gmail.com>
2026-01-23 00:10:29 +0530

    Logo setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
     <>
-      <nav>
-        <div>
+      <nav className='fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-3xl z-20 border-b'>
+        <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
             {/* {logo} */}
+            <Link href={"/"} className='flex items-center'>
+            <Image
+            src="/logo.png"
+            alt='logo-png'
+            height={500}
+            width={500}
+            className="w-full h-11 rounded-2xl"
+            priority
+            />
+            </Link>
 
             {/* {search bar and location for desktop} */}
 
