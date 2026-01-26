e39ad661181d6a9faa993615340f31831f78fb7c
Aman Patel <aman082199@gmail.com>
2026-01-27 01:36:56 +0530

    Database setup using convex

---
+++ convex/schema.js
@@ -0,0 +1,37 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+
+
+
+
+export default defineSchema({
+    //Users table
+    users: defineTable({
+          name: v.string(),
+          tokenIdentifier: v.string(),   //clerk ID fro authentication
+            email: v.string(),
+            profileImageUrl: v.optional(v.string()),
+
+            //Onboarding fields
+            hasCompletedOnboarding: v.boolean(),
+
+            //Location fields
+            location: v.string(
+                v.object({
+                    city: v.string(),
+                    state: v.optional(v.string()),
+                    country: v.string(),
+                })
+            ),
+
+            //Interest fields
+            interests: v.optional(v.array(v.string())),
+
+            //Organizer tracking 
+            freeEventsCreated: v.number(),
+
+            createdAt: v.number(),
+            updatedAt: v.number(),
+    }).index("by_token", ["tokenIdentifier"]),
+})
