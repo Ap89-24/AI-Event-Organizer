338ec5b9c700a81effc8e77d555b487d49f81dfd
Aman Patel <aman082199@gmail.com>
2026-01-27 01:06:16 +0530

    Loader

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
 import {Authenticated , Unauthenticated} from "convex/react"
+import {BarLoader} from "react-spinners"
 
 const Header = () => {
   return (
@@ -42,6 +43,11 @@ const Header = () => {
             </div>
         </div>
         {/* search and location for mobile only */}
+
+        {/* Loader */}
+        <div className='absolute bottom-0 left-0 w-full'>
+            <BarLoader width={'100%'} color='#a855f7' />
+        </div>
 
       </nav>
 
