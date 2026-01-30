dd196c9babb529e96ae981eb251967f3e93e2df6
Aman Patel <aman082199@gmail.com>
2026-01-31 02:54:09 +0530

    landing page

--- convex/schema.js
+++ convex/schema.js
@@ -79,7 +79,7 @@ export default defineSchema({
       .index("by_category", ["category"])
       .index("by_start_date" , ["startDate"])
       .index("by_slug", ["slug"])
-      .searchIndex("search_tittle" , {searchField: "'title'"}),
+      .searchIndex("search_tittle" , {searchField: "title"}),
 
     //Registrations Table...
     registrations: defineTable({
