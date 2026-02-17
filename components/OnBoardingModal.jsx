e3585fabddaf8032f39dba1f7150f035f9fbf323
Ap89-24 <aman082199@gmail.com>
2026-02-17 23:29:47 +0530

    user onboarding

--- components/OnBoardingModal.jsx
+++ components/OnBoardingModal.jsx
@@ -1,5 +1,5 @@
-"use client"
-import { Button } from "@/components/ui/button"
+"use client";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
@@ -9,77 +9,171 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Field, FieldGroup } from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useState } from "react"
-import { Progress } from "./ui/progress"
-import { Heart, MapPin } from "lucide-react"
-import { CATEGORIES } from "@/lib/data"
+} from "@/components/ui/dialog";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { Progress } from "./ui/progress";
+import { ArrowRight, Heart, MapPin } from "lucide-react";
+import { CATEGORIES } from "@/lib/data";
+import { Badge } from "./ui/badge";
+import { useConvexMutation } from "@/hooks/use-convex-query";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+import { City, State } from "country-state-city";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "./ui/select";
 
-export function OnBoardingModal({isOpen,onClose,onComplete}) {
+export function OnBoardingModal({ isOpen, onClose, onComplete }) {
+  const [step, setStep] = useState(1);
+  const progress = (step / 2) * 100;
+  const [selectedInterest, setSelectedInterest] = useState([]);
+  const [location, setLocation] = useState({
+    state: "",
+    city: "",
+    country: "India",
+  });
 
-  const [step , setStep] = useState(1);
-  const progress = (step/2) * 100;
-  
+  const { mutate: completeOnboarding, isLoading } = useConvexMutation(
+    api.users.completeOnboarding,
+  );
+
+  const IndianStates = State.getStatesOfCountry("IN");
+
+  const cities = () => {
+    if (!location.state) return [];
+    const selectedStates = IndianStates.find((s) => s.name === location.state);
+    if (!selectedStates) return [];
+    return City.getCitiesOfState("IN", selectedStates.isoCode);
+  };
+
   const toggleInterest = (categoryid) => {
+    setSelectedInterest((prev) =>
+      prev.includes(categoryid)
+        ? prev.filter((id) => id !== categoryid)
+        : [...prev, categoryid],
+    );
+  };
 
-  }
+  const handleComplete = async () => {};
+
+  const handleNext = () => {
+    if (step === 1 && selectedInterest.length < 3) {
+      toast.error("Please select atleast 3 interest");
+      return;
+    }
+    if (step === 2 && (!location.city || !location.state)) {
+      toast.error("Please atleast both state and city");
+      return;
+    }
+    if (step < 2) {
+      setStep(step + 1);
+    } else {
+      P;
+      handleComplete();
+    }
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-        <DialogContent className="sm:max-w-2xl">
-          <DialogHeader>
-            <div className="mb-4">
-              <Progress value={progress} className="h-1" />
-            </div>
-            <DialogTitle className={"flex items-center gap-3 text-2xl"}>
-              {step === 1 ? (
-                <>
+      <DialogContent className="sm:max-w-2xl">
+        <DialogHeader>
+          <div className="mb-4">
+            <Progress value={progress} className="h-1" />
+          </div>
+          <DialogTitle className={"flex items-center gap-3 text-2xl"}>
+            {step === 1 ? (
+              <>
                 <Heart className="w-6 h-6 text-purple-500" />
                 What interests you?
-                </>
-              ) : (
-                <>
+              </>
+            ) : (
+              <>
                 <MapPin className="w-6 h-6 text-purple-500" />
-                Where are you located? 
-                </>
+                Where are you located?
+              </>
+            )}
+          </DialogTitle>
+          <DialogDescription>
+            {step === 1
+              ? "Select atleast 3 categories to personalize your experience"
+              : "We'll show events happen near you"}
+          </DialogDescription>
+        </DialogHeader>
+        <div className="py-4">
+          {step === 1 && (
+            <div className="space-y-6">
+              <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 max-h-[400px] overflow-y-auto p-3">
+                {CATEGORIES.map((category) => (
+                  <button
+                    key={category.id}
+                    onClick={() => toggleInterest(category.id)}
+                    className={`p-4 rounded-lg border-2 transition-all hover:scale-110 ${
+                      selectedInterest.includes(category.id)
+                        ? "border-purple-500 bg-purple-500/10 shadow-lg shadow-purple-500/30"
+                        : "border-border hover:border-purple-400 transition-all"
+                    }`}
+                  >
+                    <div className="text-2xl mb-2.5">{category.icon}</div>
+                    <div className="text-sm font-medium">{category.label}</div>
+                  </button>
+                ))}
+              </div>
+
+              <Badge
+                variant={selectedInterest.length >= 3 ? "default" : "secondary"}
+              >
+                {selectedInterest.length} Selected
+              </Badge>
+              {selectedInterest.length >= 3 && (
+                <span className="text-sm text-purple-400 font-semibold p-2">
+                  ☑ Ready to Continue
+                </span>
               )}
-            </DialogTitle>
-            <DialogDescription>
-                {step === 1 ? 
-              "Select atleast 3 categories to personalize your experience" : 
-              "We'll show events happen near you"  
-              }
-            </DialogDescription>
-          </DialogHeader>
-          <div className="py-4">
-            {step === 1 && (
-              <div className="space-y-6">
-                <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 max-h-[400px] overflow-y-auto p-3">
-                    {CATEGORIES.map((category) => (
-                      <button key={category.id}
-                      onClick={() => toggleInterest(category.id)}
-                      className="p-4 rounded-lg border-2 transition-all hover:scale-110"
-                      >
-                        <div className="text-2xl mb-2.5">{category.icon}</div>
-                        <div className="text-sm font-medium">{category.label}</div>
-                      </button>
-                    ))}
+            </div>
+          )}
+
+          {step === 2 && (
+            <div className="space-y-6">
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <Label htmlFor="state">State</Label>
+                  <Select value={location.state}
+                  onValueChange={(value) => {
+                    setLocation({...location, state: value , city:""});
+                  }}
+                  >
+                    <SelectTrigger id="state" className="h-11 w-full">
+                      <SelectValue placeholder="Select a State" />
+                    </SelectTrigger>
+                    <SelectContent>
+                       {IndianStates.map((state) => (
+                        <SelectItem key={state.isoCode} value={state.name}>
+                            {state.name}
+                        </SelectItem>
+                       ))}
+                    </SelectContent>
+                  </Select>
                 </div>
               </div>
-            )}
-            </div>            
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-     
+            </div>
+          )}
+        </div>
+        <DialogFooter className={`flex gap-3.5`}>
+          <Button
+            className={`flex-1 gap-2`}
+            disabled={isLoading}
+            onClick={handleNext}
+          >
+            {isLoading
+              ? "Completing..."
+              : step === 2
+                ? "Complete setup"
+                : "Continue"}
+            <ArrowRight className="w-6 h-6" />
+          </Button>
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
-  )
+  );
 }
-
