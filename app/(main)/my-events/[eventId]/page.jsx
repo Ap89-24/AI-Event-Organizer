c8996dacae85c4d0b106263f35492d343412cee6
Ap89-24 <aman082199@gmail.com>
2026-04-05 22:23:24 +0530

    building event dashboard page with logic

--- app/(main)/my-events/[eventId]/page.jsx
+++ app/(main)/my-events/[eventId]/page.jsx
@@ -1,22 +1,71 @@
 "use client";
-import { useParams, useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+import { api } from "@/convex/_generated/api";
+import { useConvexQuery } from "@/hooks/use-convex-query";
+import { Loader2 } from "lucide-react";
+import { notFound, useParams, useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const EventDashboard = () => {
+  const params = useParams();
+  const router = useRouter();
+  const eventId = params.eventId;
 
-    const params = useParams();
-    const router = useRouter();
-    const eventId = params.eventId;
+  const [activeTab, setActiveTab] = useState("all");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showQrScanner, setShowQrScanner] = useState(false);
 
-    const [activeTab, setActiveTab] = useState("all");
-    const [searchQuery, setSearchQuery] = useState("");
-    const [showQrScanner , setShowQrScanner] = useState(false);
+  const { data: DashboardData, isLoading } = useConvexQuery(
+    api.dashboard.getEventDashboard,
+    { eventId },
+  );
 
-  return (
+  //? Fetch registrations
+
+  const { data: registrations, isLoading: registrationsLoading } =
+    useConvexQuery(api.registrations.getMyRegistrations, { eventId });
+
+  if (isLoading || registrationsLoading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <Loader2 className="w-10 h-10 animate-spin text-purple-500" />
+      </div>
+    );
+  }
+
+  if (!DashboardData) {
+    notFound();
+  }
+
+  const {event , stats} = DashboardData;
+
+  //! Filter registrations based on active tab and search query...
+  const filterRegistrations = registrations.filter((reg) => {
+    const matchesSearch = reg.attendeeName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        reg.attendeeEmail.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        reg.qrCode.toLowerCase().includes(searchQuery.toLowerCase());
+
+        if(activeTab === "all"){
+            return matchesSearch && reg.status === "registered";
+        };
+
+        if(activeTab === "checked-in"){
+            return matchesSearch && reg.checkedIn && reg.status === "registered";
+        };
+
+        if(activeTab === "pending"){
+            return matchesSearch && !reg.checkedIn && reg.status === "registered";
+        };
+
+        return matchesSearch;
+  });
+
+  return <div>
     <div>
-      
+        
     </div>
-  )
-}
 
-export default EventDashboard
+    {/* OR Scanner model */}
+  </div>;
+};
+
+export default EventDashboard;
